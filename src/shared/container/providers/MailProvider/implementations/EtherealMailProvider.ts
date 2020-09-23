f61bdab022e72ef1a31cf3cf45e3525652f2d958
Luiz <luiz.ado@hotmail.com>
2020-09-23 18:51:44 -0300

    Added the EtherealMailProvider implementation

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,7 @@
+import IMailProvider from '../models/IMailProvider';
+
+export default class EtherealMailProvider implements IMailProvider {
+  public async sendMail(to: string, body: string): Promise<void> {
+    
+  }
+}
