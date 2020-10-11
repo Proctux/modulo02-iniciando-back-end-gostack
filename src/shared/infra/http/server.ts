8cad7c3f2483c2a74334721f77e49b0a9920ab20
Luiz <luiz.ado@hotmail.com>
2020-10-11 18:30:35 -0300

    Added celebrate lib to the project, validating the requests as a middleware.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import { errors } from 'celebrate';
+
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
@@ -16,6 +18,8 @@ const app = express();
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if(err instanceof AppError) {
